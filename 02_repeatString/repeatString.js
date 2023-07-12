5dbc09e511c4f96783480a1f0e76c4afa896a010
MM-AUT <138686330+MM-AUT@users.noreply.github.com>
2023-07-12 15:01:32 +0200

    Solve 1-2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if (number <0) {
+        return "ERROR"
+    }
+    let string = ""
+    for (let i=1; i <= number; i++) {
+        string = string + word
+    }
+    return string
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(min,max) {
+    let sum = 0
+    for (let i = min; i < max; i++)
+    return sum =+ i
 };
 
 // Do not edit below this line
