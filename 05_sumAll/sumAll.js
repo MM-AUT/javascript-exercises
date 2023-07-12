de2941f57f620f42c341ab325a34aa4d20787fc7
MM-AUT <138686330+MM-AUT@users.noreply.github.com>
2023-07-12 15:39:14 +0200

    finish 5-6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,15 @@
-const sumAll = function(min,max) {
+const sumAll = function(min, max) {
     let sum = 0
-    for (let i = min; i < max; i++)
-    return sum =+ i
-};
+    if (min > max) {
+        const temp = max
+        max = min
+        min = temp
+    }
+    for (let i = min; i < max + 1; i++) {
+        sum += i;
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
